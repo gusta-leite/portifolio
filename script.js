2a7ac6fcbaab6e8341bcf9ceb8ccaa7cb776bc27
Gustavo Leite <164664879+gusta-leite@users.noreply.github.com>
2024-11-28 16:57:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.documentElement.style.cursor = 'none';
+
+// gsap 
+
+gsap.set(".star", {xPercent: -50, yPercent: -50});
+
+let xTo = gsap.quickTo(".star", "x", {duration: 0.4, ease: "power3"}),
+yTo = gsap.quickTo(".star", "y", {duration: 0.4, ease: "power3"});
+
+window.addEventListener("mousemove", e => {
+xTo(e.clientX);
+yTo(e.clientY);
+
+});
+
+gsap.to(".star", {
+    rotation: 360,
+    duration: 5,
+    ease: "power1.inOut",
+    repeat: -1
+  });
+
