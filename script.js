9ba63e4e9b43fe093840f0ca42318993b5ee804d
Gustavo Leite <164664879+gusta-leite@users.noreply.github.com>
2024-12-13 22:18:26 -0300

    update - projects and new footer

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.documentElement.style.cursor = 'none';
 
-// gsap 
+// gsap to cursor
 
-gsap.set(".star", {xPercent: -50, yPercent: -50});
+gsap.set(".cursor", {xPercent: -50, yPercent: -50});
 
-let xTo = gsap.quickTo(".star", "x", {duration: 0.4, ease: "power3"}),
-yTo = gsap.quickTo(".star", "y", {duration: 0.4, ease: "power3"});
+let xTo = gsap.quickTo(".cursor", "x", {duration: 0.4, ease: "power3"}),
+yTo = gsap.quickTo(".cursor", "y", {duration: 0.4, ease: "power3"});
 
 window.addEventListener("mousemove", e => {
 xTo(e.clientX);
@@ -13,9 +13,19 @@ yTo(e.clientY);
 
 });
 
-gsap.to(".star", {
+gsap.to(".cursor", {
     rotation: 360,
     duration: 5,
     ease: "power1.inOut",
     repeat: -1
   });
+
+// gsap rotation
+
+gsap.to(".star", {
+  rotation: 360,
+  duration: 20,
+  ease: "linear",
+  repeat: -1,
+  transformOrigin: "50% 50%"
+});
