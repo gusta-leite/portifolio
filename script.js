38f579748c382dbf38fdbd908dfe9663a7a66376
Gustavo Leite <164664879+gusta-leite@users.noreply.github.com>
2024-12-16 02:13:14 -0300

    update

--- script.js
+++ script.js
@@ -2,23 +2,22 @@ document.documentElement.style.cursor = 'none';
 
 // gsap to cursor
 
-gsap.set(".cursor", {xPercent: -50, yPercent: -50});
 
-let xTo = gsap.quickTo(".cursor", "x", {duration: 0.4, ease: "power3"}),
-yTo = gsap.quickTo(".cursor", "y", {duration: 0.4, ease: "power3"});
+gsap.set(".cursor, .cursor2", { xPercent: -50, yPercent: -50 });
 
-window.addEventListener("mousemove", e => {
-xTo(e.clientX);
-yTo(e.clientY);
+let xToCursor = gsap.quickTo(".cursor", "x", { duration: 0.4, ease: "power3" }),
+    yToCursor = gsap.quickTo(".cursor", "y", { duration: 0.4, ease: "power3" });
 
+let xToCursor2 = gsap.quickTo(".cursor2", "x", { duration: 0.6, ease: "power3" }), 
+    yToCursor2 = gsap.quickTo(".cursor2", "y", { duration: 0.6, ease: "power3" });
+
+window.addEventListener("mousemove", (e) => {
+  xToCursor(e.clientX); 
+  yToCursor(e.clientY);
+
+  xToCursor2(e.clientX);
+  yToCursor2(e.clientY);
 });
-
-gsap.to(".cursor", {
-    rotation: 360,
-    duration: 5,
-    ease: "power1.inOut",
-    repeat: -1
-  });
 
 // gsap rotation
 
@@ -29,3 +28,24 @@ gsap.to(".star", {
   repeat: -1,
   transformOrigin: "50% 50%"
 });
+
+// botão
+
+const $ = (s, o = document) => o.querySelector(s);
+const $$ = (s, o = document) => o.querySelectorAll(s);
+
+$$('.button').forEach(el => el.addEventListener('mousemove', function(e) {
+  const pos = this.getBoundingClientRect();
+  const mx = e.clientX - pos.left - pos.width/2; 
+  const my = e.clientY - pos.top - pos.height/2;
+   
+  this.style.transform = 'translate('+ mx * 0.15 +'px, '+ my * 0.3 +'px)';
+  this.style.transform += 'rotate3d('+ mx * -0.1 +', '+ my * -0.3 +', 0, 12deg)';
+  this.children[0].style.transform = 'translate('+ mx * 0.025 +'px, '+ my * 0.075 +'px)';
+}));
+
+$$('.button').forEach(el => el.addEventListener('mouseleave', function() {
+  this.style.transform = 'translate3d(0px, 0px, 0px)';
+  this.style.transform += 'rotate3d(0, 0, 0, 0deg)';
+  this.children[0].style.transform = 'translate3d(0px, 0px, 0px)';
+}));
