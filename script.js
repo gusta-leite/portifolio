ec9356b1f911df291e2076e6434dea5d57584436
Gustavo Leite <164664879+gusta-leite@users.noreply.github.com>
2024-12-18 02:42:55 -0300

    more changes, more responsive

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ document.documentElement.style.cursor = 'none';
 gsap.set(".cursor, .cursor2", { xPercent: -50, yPercent: -50 });
 
 let xToCursor = gsap.quickTo(".cursor", "x", { duration: 0.4, ease: "power3" }),
-    yToCursor = gsap.quickTo(".cursor", "y", { duration: 0.4, ease: "power3" });
+  yToCursor = gsap.quickTo(".cursor", "y", { duration: 0.4, ease: "power3" });
 
-let xToCursor2 = gsap.quickTo(".cursor2", "x", { duration: 0.6, ease: "power3" }), 
-    yToCursor2 = gsap.quickTo(".cursor2", "y", { duration: 0.6, ease: "power3" });
+let xToCursor2 = gsap.quickTo(".cursor2", "x", { duration: 0.6, ease: "power3" }),
+  yToCursor2 = gsap.quickTo(".cursor2", "y", { duration: 0.6, ease: "power3" });
 
 window.addEventListener("mousemove", (e) => {
-  xToCursor(e.clientX); 
+  xToCursor(e.clientX);
   yToCursor(e.clientY);
 
   xToCursor2(e.clientX);
@@ -33,17 +33,17 @@ gsap.to(".star", {
 const $ = (s, o = document) => o.querySelector(s);
 const $$ = (s, o = document) => o.querySelectorAll(s);
 
-$$('.button').forEach(el => el.addEventListener('mousemove', function(e) {
+$$('.button').forEach(el => el.addEventListener('mousemove', function (e) {
   const pos = this.getBoundingClientRect();
-  const mx = e.clientX - pos.left - pos.width/2; 
-  const my = e.clientY - pos.top - pos.height/2;
-   
-  this.style.transform = 'translate('+ mx * 0.15 +'px, '+ my * 0.3 +'px)';
-  this.style.transform += 'rotate3d('+ mx * -0.1 +', '+ my * -0.3 +', 0, 10deg)';
-  this.children[0].style.transform = 'translate('+ mx * 0.025 +'px, '+ my * 0.075 +'px)';
+  const mx = e.clientX - pos.left - pos.width / 2;
+  const my = e.clientY - pos.top - pos.height / 2;
+
+  this.style.transform = 'translate(' + mx * 0.15 + 'px, ' + my * 0.3 + 'px)';
+  this.style.transform += 'rotate3d(' + mx * -0.1 + ', ' + my * -0.3 + ', 0, 10deg)';
+  this.children[0].style.transform = 'translate(' + mx * 0.025 + 'px, ' + my * 0.075 + 'px)';
 }));
 
-$$('.button').forEach(el => el.addEventListener('mouseleave', function() {
+$$('.button').forEach(el => el.addEventListener('mouseleave', function () {
   this.style.transform = 'translate3d(0px, 0px, 0px)';
   this.style.transform += 'rotate3d(0, 0, 0, 0deg)';
   this.children[0].style.transform = 'translate3d(0px, 0px, 0px)';
